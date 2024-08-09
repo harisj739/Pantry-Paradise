f72e5aab6c875f2bec6084a14ad88fd143950d7d
harisj739 <hjilani@csumb.edu>
2024-08-08 23:46:30 -0700

    Re-styled pages and added a search feature to it.

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import {firestore} from "@/firebase";
 import {Box, Button, Modal, Stack, TextField, Typography} from "@mui/material";
 import {doc, getDoc, setDoc, deleteDoc, getDocs, collection, query} from 'firebase/firestore';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import SearchBar from 'search-bar-react';
 import Head from "next/head";
 import {Helmet} from "react-helmet";
 
@@ -12,6 +13,7 @@ export default function Home() {
   const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState('');
+  const [searchQuery, setSearchQuery] = useState(''); // State for search query
 
   const updateInventory = async() => {
     const snapshot = query(collection(firestore, 'inventory'));
@@ -53,6 +55,11 @@ export default function Home() {
     await updateInventory();
   }
 
+    // Filtering the inventory based on search query
+    const filteredInventory = inventory.filter(item =>
+        item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
   const removeItem = async(item) => {
     const docRef = doc(collection(firestore, 'inventory'), item);
     const docSnap = await getDoc(docRef);
@@ -79,7 +86,7 @@ export default function Home() {
   const handleClose = () => setOpen(false);
 
   return (
-    <Box width={"100vw"} height={"100vh"} display={"flex"} flexDirection="column" justifyContent="center" alignItems={'center'} gap={2} overflow={"auto"} sx={{
+    <Box width={"100vw"} height={"100vh"} bgcolor="#F2D2BD" display={"flex"} flexDirection="column" justifyContent="center" alignItems={'center'} gap={2} overflow={"auto"} sx={{
           gap: '16px', // Use gap to control spacing between children
           overflowX: 'hidden', // Horizontal scrolling if needed
           overflowY: 'auto' // Hide vertical scrolling if not needed
@@ -99,16 +106,29 @@ export default function Home() {
           </Stack>
         </Box>
       </Modal>
-      <Button
-          variant={"contained"}
-          onClick={() => {
-            handleOpen()
-          }}> Add New Item </Button>
       {/*<Box border={"1px solid #fff"}>*/}
-        <Box width={"800px"} height={"100px"} bgcolor={"#1E90FF"} display={"flex"} alignItems={"center"} justifyContent={"center"}>
-          <Typography variant={"h2"} color={'#fff'}>
+        <Box width={"800px"} height={"100px"} display={"flex"} alignItems={"center"} justifyContent={"center"}>
+          <Typography variant={"h2"} color={'#80461B'}>
             Pantry Paradise
           </Typography>
+        </Box>
+        <Box width={"1000px"} height={"43px"} display={"flex"} flexDirection="row">
+            <SearchBar
+                onChange={(text) => setSearchQuery(text)}
+                onFocus={() => console.log('focused')}
+                size='large'
+                width='100%'
+                autoFocus
+                placeholder='Search Pantry Item'
+                onClear={() => setSearchQuery('')} // Clear search query on clear
+                value={searchQuery} // Set the value to the current search query
+            />
+            <Button
+                variant={"contained"}
+                color={"success"}
+                onClick={() => {
+                    handleOpen()
+                }}> <i class="bi bi-plus-circle" style={{paddingRight: "10px"}}></i> Add </Button>
         </Box>
       <Box display='flex' flexWrap='wrap' flexDirection={'row'} justifyContent="center" spacing={2} overflow={"auto"} paddingLeft={'50px'} sx={{
         gap: '30px', // Use gap to control spacing between children
@@ -116,14 +136,15 @@ export default function Home() {
         overflowY: 'auto' // Hide vertical scrolling if not needed
       }}>
         {
-          inventory.map(({name, quantity}) => (
-              <Box key={"name"} minHeight={"150px"} display={"flex"} alignItems={"center"} bgColor={"#f0f0f0"} padding={5} sx={{
+            filteredInventory.map(({name, quantity}) => (
+              <Box key={"name"} minHeight={"150px"} display={"flex"} alignItems={"center"} padding={10} sx={{
                 borderRadius: '16px', // Adjust the value as needed for rounder edges
-                border: '1px solid #336BFF',  // Optional: Add border to visualize the rounded corners
+                border: '1px solid #7B3F00',  // Optional: Add border to visualize the rounded corners
+                  paddingTop: '5px',
                 padding: '16px',      // Optional: Add padding for spacing
                 margin: '8px',        // Optional: Add margin between items
-                bgcolor: '#339BFF',
-                boxShadow: '0px 4px 8px #336BFF',
+                bgcolor: '#CD7F32',
+                boxShadow: '0px 4px 8px #7B3F00',
                 overflow: 'auto', // Handles overflow content
               }}>
                 <Typography variant={"h4"} color={"#fff"} textAlign={"center"} paddingRight={"50px"}>
